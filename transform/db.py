9caeb937f84d4d6fc81095b8c2fb1190656b677b
siarkonyar <siarkonyar@gmail.com>
2026-08-23 15:19:08 +0100

    feat: create the engine to connect to destination database

--- transform/db.py
+++ transform/db.py
@@ -8,3 +8,13 @@ PORT = os.environ.get("DESTINATION_POSTGRES_PORT", "5432")
 DB_NAME = os.environ.get("DESTINATION_POSTGRES_DB", "destination_db")
 USER = os.environ.get("DESTINATION_POSTGRES_USER", "postgres")
 PASSWORD = os.environ.get("DESTINATION_POSTGRES_PASSWORD")
+
+def get_engine():
+  if not PASSWORD:
+      raise RuntimeError(
+          "DESTINATION_POSTGRES_PASSWORD is not set. "
+          "Check that docker-compose.yaml passes ./elt_script/.env to this service."
+      )
+
+  url = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
+  return create_engine(url)
