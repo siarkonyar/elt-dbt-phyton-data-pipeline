b64643fec78dd0936952c90c56682121c4f5143e
siarkonyar <siarkonyar@gmail.com>
2026-09-23 16:01:52 +0100

    feat: implement hash_password helper function

--- api/passwords.py
+++ api/passwords.py
@@ -1,2 +1,19 @@
+import bcrypt
+
 BCRYPT_ROUNDS = 12
 MAX_PASSWORD_BYTES = 72
+
+def hash_password(password, rounds=BCRYPT_ROUNDS):
+    if not password:
+        raise ValueError
+
+    encoded_password = password.encode("utf-8")
+
+    if len(encoded_password) > MAX_PASSWORD_BYTES:
+        raise ValueError
+
+    salt = bcrypt.gensalt(rounds=rounds)
+
+    hashed_password = bcrypt.hashpw(encoded_password, salt).decode()
+
+    return hashed_password
